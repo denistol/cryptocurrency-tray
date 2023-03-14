1aab7a268d36f4ce0ae8668796d29b715488f805
denistol <denistol@pm.me>
2023-03-14 15:22:20 +0200

    Initial Commit

---
+++ cryptocurrency-tray/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  cryptocurrency-tray
+//
+//  Created by Denis on 14.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
